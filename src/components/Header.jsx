a1130e7d6cbf891bfc3779e2bc8ab0b2a55dae72
Angus Perkerson <angusp@angusp.com>
2020-03-08 15:49:35 -0700

    feat: adds grommet themes
    Adds grommet themes for style, and cleans up styles for the temes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,53 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { Header as GHeader, Nav, Anchor, Heading } from 'grommet';
+import React, { useCallback } from 'react';
+import { Header as GHeader, Nav, Anchor, Heading, CheckBox } from 'grommet';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-const Header = ({ siteTitle }) => (
-  <GHeader as="header" background="brand" flex pad="medium" justify="between" role="banner">
-    <Heading as="div">
-      <Anchor as={Link} to="/" color="white">
-        {siteTitle}
-      </Anchor>
-    </Heading>
-    <Nav direction="row" background="brand" role="navigation">
-      <Anchor as={Link} to="/page-2" color="white">
-        Page 2
-      </Anchor>
-      <Anchor
-        href="https://twitter.com/angusp"
-        a11yTitle="Angus's Twitter Account"
-        icon={<FontAwesomeIcon icon={faTwitter} />}
-        color="white"
-      />
-    </Nav>
-  </GHeader>
-);
+export const THEME_LIGHT = 'Light';
+export const THEME_DARK = 'Dark';
+
+function Header({ siteTitle, onThemeToggle, theme }) {
+  const onThemeChange = useCallback(() => {
+    const newTheme = theme === THEME_LIGHT ? THEME_DARK : THEME_LIGHT;
+    onThemeToggle(newTheme);
+  }, [onThemeToggle]);
+
+  return (
+    <GHeader as="header" flex pad="medium" justify="between" role="banner" background="brand">
+      <Heading as="div">
+        <Anchor as={Link} to="/" label={siteTitle} />
+      </Heading>
+      <Nav direction="row" role="navigation">
+        <Anchor as={Link} to="/page-2">
+          Page 2
+        </Anchor>
+        <Anchor
+          href="https://twitter.com/angusp"
+          a11yTitle="Angus's Twitter Account"
+          icon={<FontAwesomeIcon icon={faTwitter} />}
+        />
+      </Nav>
+      <div>
+        Theme:
+        {' '}
+        <CheckBox toggle checked={theme === THEME_LIGHT} label={theme} onClick={onThemeChange} />
+      </div>
+    </GHeader>
+  );
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  onThemeToggle: PropTypes.func,
+  theme: PropTypes.string,
 };
 
 Header.defaultProps = {
   siteTitle: ``,
+  onThemeToggle: () => {},
+  theme: THEME_LIGHT,
 };
 
 export default Header;
