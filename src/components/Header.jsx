9c8ab9f9445334e36502bcc648aac666acdd9cc3
Angus Perkerson <angusp@angusp.com>
2020-03-07 23:14:18 -0800

    feat: adds styled-components
    Adds the styled-components library for react styling
    
    closes #14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,36 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`;
+
+const NavLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
+const Headline = styled.h1`
+  margin: 0;
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Container>
+    <Wrapper>
+      <Headline style={{ margin: 0 }}>
+        <NavLink to="/">{siteTitle}</NavLink>
+      </Headline>
+    </Wrapper>
+  </Container>
 );
 
 Header.propTypes = {
