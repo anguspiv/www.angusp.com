91c65933ac1d7a15f1b5200a13a75fc8975aba07
Angus Perkerson <angusp@angusp.com>
2020-03-08 15:49:35 -0700

    feat: adds grommet themes
    Adds grommet themes for style, and cleans up styles for the temes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,17 +5,36 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
-import { Reset } from 'styled-reset';
-import { Grommet, Main, Footer, Text } from 'grommet';
+import { Grommet, Main, Footer, Text, Anchor } from 'grommet';
+import deepMerge from 'deepmerge';
+import { createGlobalStyle } from 'styled-components';
 
-import theme from '../styles/theme';
+import lightTheme from '../styles/themes/light';
+import darkTheme from '../styles/themes/dark';
+import defaultTheme from '../styles/theme';
 
-import Header from './Header';
+import Header, { THEME_DARK, THEME_LIGHT } from './Header';
+
+const dark = deepMerge(defaultTheme, darkTheme);
+const light = deepMerge(defaultTheme, lightTheme);
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    transition: background .5s ease-in-out .1s, color .5s ease-in-out .1s;
+  }
+`;
 
 const Layout = ({ children }) => {
+  const [theme, setTheme] = useState(THEME_LIGHT);
+
+  const onThemeToggle = useCallback(themeLabel => {
+    setTheme(themeLabel);
+  });
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -27,20 +46,26 @@ const Layout = ({ children }) => {
   `);
 
   return (
-    <Grommet theme={theme}>
-      <Reset />
-      <Header siteTitle={data.site.siteMetadata.title} />
+    <Grommet theme={theme === THEME_DARK ? dark : light}>
+      <GlobalStyle />
+      <Header
+        siteTitle={data.site.siteMetadata.title}
+        theme={theme}
+        onThemeToggle={onThemeToggle}
+      />
       <Main pad="medium" as="main" role="main">
         {children}
       </Main>
-      <Footer pad="medium" role="contentinfo">
-        <Text>
+      <Footer pad="medium" role="contentinfo" background="accent-1">
+        <Text color="white">
           © 
           {' '}
           {new Date().getFullYear()}
-          , Built with 
+          , Built with
           {' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <Anchor href="https://www.gatsbyjs.org" color="white">
+            Gatsby
+          </Anchor>
         </Text>
       </Footer>
     </Grommet>
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,20 +1,5 @@
 const theme = {
   global: {
-    colors: {
-      brand: '#214397',
-      accent: ['#b41443', '#43b414', '#FFFFFF'],
-    },
-    input: {
-      border: {
-        radius: '4px',
-      },
-    },
-    font: {
-      name: 'Fira Sans',
-      family: "'Fira Sans', Arial, sans-serif",
-      face:
-        "/* cyrillic-ext */\n@font-face {\n  font-family: 'Fira Sans';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Fira Sans Regular'), local('FiraSans-Regular'), url(https://fonts.gstatic.com/s/firasans/v10/va9E4kDNxMZdWfMOD5VvmojLazX3dGTP.woff2) format('woff2');\n  unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;\n}\n/* cyrillic */\n@font-face {\n  font-family: 'Fira Sans';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Fira Sans Regular'), local('FiraSans-Regular'), url(https://fonts.gstatic.com/s/firasans/v10/va9E4kDNxMZdWfMOD5Vvk4jLazX3dGTP.woff2) format('woff2');\n  unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;\n}\n/* greek-ext */\n@font-face {\n  font-family: 'Fira Sans';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Fira Sans Regular'), local('FiraSans-Regular'), url(https://fonts.gstatic.com/s/firasans/v10/va9E4kDNxMZdWfMOD5Vvm4jLazX3dGTP.woff2) format('woff2');\n  unicode-range: U+1F00-1FFF;\n}\n/* greek */\n@font-face {\n  font-family: 'Fira Sans';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Fira Sans Regular'), local('FiraSans-Regular'), url(https://fonts.gstatic.com/s/firasans/v10/va9E4kDNxMZdWfMOD5VvlIjLazX3dGTP.woff2) format('woff2');\n  unicode-range: U+0370-03FF;\n}\n/* vietnamese */\n@font-face {\n  font-family: 'Fira Sans';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Fira Sans Regular'), local('FiraSans-Regular'), url(https://fonts.gstatic.com/s/firasans/v10/va9E4kDNxMZdWfMOD5VvmIjLazX3dGTP.woff2) format('woff2');\n  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;\n}\n/* latin-ext */\n@font-face {\n  font-family: 'Fira Sans';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Fira Sans Regular'), local('FiraSans-Regular'), url(https://fonts.gstatic.com/s/firasans/v10/va9E4kDNxMZdWfMOD5VvmYjLazX3dGTP.woff2) format('woff2');\n  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;\n}\n/* latin */\n@font-face {\n  font-family: 'Fira Sans';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Fira Sans Regular'), local('FiraSans-Regular'), url(https://fonts.gstatic.com/s/firasans/v10/va9E4kDNxMZdWfMOD5Vvl4jLazX3dA.woff2) format('woff2');\n  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;\n}\n",
-    },
     button: {
       border: {
         radius: '4px',
@@ -29,6 +14,636 @@ const theme = {
       border: {
         radius: '4px',
       },
+    },
+    animation: {
+      duration: '1s',
+      jiggle: {
+        duration: '0.1s',
+      },
+    },
+    borderSize: {
+      xsmall: '1px',
+      small: '2px',
+      medium: '4px',
+      large: '12px',
+      xlarge: '24px',
+    },
+    breakpoints: {
+      small: {
+        value: 768,
+        borderSize: {
+          xsmall: '1px',
+          small: '2px',
+          medium: '4px',
+          large: '6px',
+          xlarge: '12px',
+        },
+        edgeSize: {
+          none: '0px',
+          hair: '1px',
+          xxsmall: '2px',
+          xsmall: '3px',
+          small: '6px',
+          medium: '12px',
+          large: '24px',
+          xlarge: '48px',
+        },
+        size: {
+          xxsmall: '2px',
+          xsmall: '2px',
+          small: '4px',
+          medium: '8px',
+          large: '16px',
+          xlarge: '32px',
+          full: '100%',
+          none: '0',
+          hair: '1px',
+        },
+      },
+      medium: {
+        value: 1536,
+      },
+      large: {},
+    },
+    deviceBreakpoints: {
+      phone: 'small',
+      tablet: 'medium',
+      computer: 'large',
+    },
+    control: {
+      border: {
+        width: '1px',
+        radius: '4px',
+      },
+    },
+    debounceDelay: 300,
+    drop: {
+      border: {
+        width: '0px',
+        radius: '4px',
+      },
+      shadowSize: 'small',
+      zIndex: '20',
+    },
+    edgeSize: {
+      none: '0',
+      hair: '1px',
+      xxsmall: '2px',
+      xsmall: '4px',
+      small: '8px',
+      medium: '16px',
+      large: '32px',
+      xlarge: '64px',
+      responsiveBreakpoint: 'small',
+    },
+    focus: {},
+    font: {
+      size: '14px',
+      height: '24px',
+      maxWidth: '432px',
+      family: "'Montserrat', sans-serif;",
+    },
+    hover: {
+      background: {},
+      color: {},
+    },
+    input: {
+      padding: '12px',
+      weight: 600,
+      border: {
+        radius: '4px',
+      },
+    },
+    selected: {},
+    spacing: '24px',
+    size: {
+      xxsmall: '48px',
+      xsmall: '96px',
+      small: '192px',
+      medium: '384px',
+      large: '768px',
+      xlarge: '1152px',
+      xxlarge: '1536px',
+      full: '100%',
+    },
+  },
+  icon: {
+    size: {
+      small: '12px',
+      medium: '24px',
+      large: '48px',
+      xlarge: '96px',
+      xsmall: '14px',
+    },
+  },
+  accordion: {
+    icons: {},
+  },
+  anchor: {
+    textDecoration: 'none',
+    fontWeight: 600,
+    hover: {
+      textDecoration: 'underline',
+    },
+  },
+  box: {
+    responsiveBreakpoint: 'small',
+  },
+  button: {
+    border: {
+      width: '2px',
+      radius: '1px',
+    },
+    disabled: {
+      opacity: 0.3,
+    },
+    minWidth: '96px',
+    maxWidth: '384px',
+    padding: {
+      vertical: '4px',
+      horizontal: '22px',
+    },
+  },
+  calendar: {
+    small: {
+      fontSize: '14px',
+      lineHeight: 1.375,
+      daySize: '27.428571428571427px',
+      slideDuration: '0.2s',
+    },
+    medium: {
+      fontSize: '18px',
+      lineHeight: 1.45,
+      daySize: '54.857142857142854px',
+      slideDuration: '0.5s',
+    },
+    large: {
+      fontSize: '30px',
+      lineHeight: 1.11,
+      daySize: '109.71428571428571px',
+      slideDuration: '0.8s',
+    },
+    icons: {
+      small: {},
+    },
+  },
+  carousel: {
+    icons: {},
+  },
+  chart: {},
+  checkBox: {
+    border: {
+      width: '2px',
+    },
+    check: {
+      radius: '4px',
+      thickness: '4px',
+    },
+    icon: {},
+    icons: {},
+    hover: {
+      border: {},
+    },
+    size: '24px',
+    toggle: {
+      radius: '24px',
+      size: '48px',
+      knob: {},
+    },
+  },
+  clock: {
+    analog: {
+      hour: {
+        width: '8px',
+        size: '24px',
+        shape: 'round',
+      },
+      minute: {
+        width: '4px',
+        size: '12px',
+        shape: 'round',
+      },
+      second: {
+        width: '3px',
+        size: '9px',
+        shape: 'round',
+      },
+      size: {
+        small: '72px',
+        medium: '96px',
+        large: '144px',
+        xlarge: '216px',
+        huge: '288px',
+      },
+    },
+    digital: {
+      text: {
+        xsmall: {
+          size: '10px',
+          height: 1.5,
+        },
+        small: {
+          size: '14px',
+          height: 1.43,
+        },
+        medium: {
+          size: '18px',
+          height: 1.375,
+        },
+        large: {
+          size: '22px',
+          height: 1.167,
+        },
+        xlarge: {
+          size: '26px',
+          height: 1.1875,
+        },
+        xxlarge: {
+          size: '34px',
+          height: 1.125,
+        },
+      },
+    },
+  },
+  collapsible: {
+    minSpeed: 200,
+    baseline: 500,
+  },
+  dataTable: {
+    header: {},
+    groupHeader: {
+      border: {
+        side: 'bottom',
+        size: 'xsmall',
+      },
+      fill: 'vertical',
+      pad: {
+        horizontal: 'small',
+        vertical: 'xsmall',
+      },
+    },
+    icons: {},
+    resize: {
+      border: {
+        side: 'right',
+      },
+    },
+    primary: {
+      weight: 'bold',
+    },
+  },
+  diagram: {
+    line: {},
+  },
+  formField: {
+    border: {
+      position: 'inner',
+      side: 'bottom',
+      error: {},
+    },
+    content: {
+      pad: {
+        horizontal: 'small',
+        bottom: 'small',
+      },
+    },
+    error: {
+      margin: {
+        vertical: 'xsmall',
+        horizontal: 'small',
+      },
+    },
+    help: {
+      margin: {
+        left: 'small',
+      },
+    },
+    label: {
+      margin: {
+        vertical: 'xsmall',
+        horizontal: 'small',
+      },
+    },
+    margin: {
+      bottom: 'small',
+    },
+  },
+  grommet: {},
+  heading: {
+    font: false,
+    level: {
+      '1': {
+        font: {},
+        small: {
+          size: '34px',
+          height: '40px',
+          maxWidth: '816px',
+        },
+        medium: {
+          size: '50px',
+          height: '56px',
+          maxWidth: '1200px',
+        },
+        large: {
+          size: '82px',
+          height: '88px',
+          maxWidth: '1968px',
+        },
+        xlarge: {
+          size: '114px',
+          height: '120px',
+          maxWidth: '2736px',
+        },
+      },
+      '2': {
+        font: {},
+        small: {
+          size: '26px',
+          height: '32px',
+          maxWidth: '624px',
+        },
+        medium: {
+          size: '34px',
+          height: '40px',
+          maxWidth: '816px',
+        },
+        large: {
+          size: '50px',
+          height: '56px',
+          maxWidth: '1200px',
+        },
+        xlarge: {
+          size: '66px',
+          height: '72px',
+          maxWidth: '1584px',
+        },
+      },
+      '3': {
+        font: {},
+        small: {
+          size: '22px',
+          height: '28px',
+          maxWidth: '528px',
+        },
+        medium: {
+          size: '26px',
+          height: '32px',
+          maxWidth: '624px',
+        },
+        large: {
+          size: '34px',
+          height: '40px',
+          maxWidth: '816px',
+        },
+        xlarge: {
+          size: '42px',
+          height: '48px',
+          maxWidth: '1008px',
+        },
+      },
+      '4': {
+        font: {},
+        small: {
+          size: '18px',
+          height: '24px',
+          maxWidth: '432px',
+        },
+        medium: {
+          size: '18px',
+          height: '24px',
+          maxWidth: '432px',
+        },
+        large: {
+          size: '18px',
+          height: '24px',
+          maxWidth: '432px',
+        },
+        xlarge: {
+          size: '18px',
+          height: '24px',
+          maxWidth: '432px',
+        },
+      },
+      '5': {
+        font: {},
+        small: {
+          size: '16px',
+          height: '22px',
+          maxWidth: '384px',
+        },
+        medium: {
+          size: '16px',
+          height: '22px',
+          maxWidth: '384px',
+        },
+        large: {
+          size: '16px',
+          height: '22px',
+          maxWidth: '384px',
+        },
+        xlarge: {
+          size: '16px',
+          height: '22px',
+          maxWidth: '384px',
+        },
+      },
+      '6': {
+        font: {},
+        small: {
+          size: '14px',
+          height: '20px',
+          maxWidth: '336px',
+        },
+        medium: {
+          size: '14px',
+          height: '20px',
+          maxWidth: '336px',
+        },
+        large: {
+          size: '14px',
+          height: '20px',
+          maxWidth: '336px',
+        },
+        xlarge: {
+          size: '14px',
+          height: '20px',
+          maxWidth: '336px',
+        },
+      },
+    },
+    responsiveBreakpoint: 'small',
+    weight: 500,
+  },
+  layer: {
+    border: {
+      radius: '4px',
+    },
+    container: {
+      zIndex: '15',
+    },
+    responsiveBreakpoint: 'small',
+    zIndex: '10',
+  },
+  menu: {
+    icons: {},
+  },
+  paragraph: {
+    small: {
+      size: '14px',
+      height: '20px',
+      maxWidth: '336px',
+    },
+    medium: {
+      size: '18px',
+      height: '24px',
+      maxWidth: '432px',
+    },
+    large: {
+      size: '22px',
+      height: '28px',
+      maxWidth: '528px',
+    },
+    xlarge: {
+      size: '26px',
+      height: '32px',
+      maxWidth: '624px',
+    },
+    xxlarge: {
+      size: '34px',
+      height: '40px',
+      maxWidth: '816px',
+    },
+  },
+  radioButton: {
+    border: {
+      width: '2px',
+    },
+    check: {
+      radius: '100%',
+    },
+    hover: {
+      border: {},
+    },
+    icon: {},
+    icons: {},
+    gap: 'small',
+    size: '24px',
+  },
+  rangeInput: {
+    track: {
+      height: '4px',
+    },
+    thumb: {},
+  },
+  rangeSelector: {},
+  select: {
+    container: {},
+    control: {},
+    icons: {},
+    options: {
+      box: {
+        align: 'start',
+        pad: 'small',
+      },
+      text: {
+        margin: 'none',
+      },
+    },
+    step: 20,
+  },
+  tab: {
+    border: {
+      side: 'bottom',
+      size: 'small',
+      active: {},
+      hover: {},
+    },
+    hover: {},
+    margin: {
+      vertical: 'xxsmall',
+      horizontal: 'small',
+    },
+    pad: {
+      bottom: 'xsmall',
+    },
+  },
+  tabs: {
+    header: {},
+    panel: {},
+  },
+  table: {
+    header: {
+      align: 'start',
+      pad: {
+        horizontal: 'small',
+        vertical: 'xsmall',
+      },
+      border: 'bottom',
+      verticalAlign: 'bottom',
+      fill: 'vertical',
+    },
+    body: {
+      align: 'start',
+      pad: {
+        horizontal: 'small',
+        vertical: 'xsmall',
+      },
+    },
+    footer: {
+      align: 'start',
+      pad: {
+        horizontal: 'small',
+        vertical: 'xsmall',
+      },
+      border: 'top',
+      verticalAlign: 'top',
+      fill: 'vertical',
+    },
+  },
+  text: {
+    xsmall: {
+      size: '10px',
+      height: 1.5,
+      maxWidth: '288px',
+    },
+    small: {
+      size: '12px',
+      height: 1.43,
+      maxWidth: '336px',
+    },
+    medium: {
+      size: '14px',
+      height: 1.375,
+      maxWidth: '432px',
+    },
+    large: {
+      size: '20px',
+      height: 1.167,
+      maxWidth: '528px',
+    },
+    xlarge: {
+      size: '28px',
+      height: 1.1875,
+      maxWidth: '624px',
+    },
+    xxlarge: {
+      size: '26px',
+      height: 1.125,
+      maxWidth: '816px',
+    },
+  },
+  video: {
+    icons: {},
+  },
+  worldMap: {
+    continent: {
+      active: '8px',
+      base: '6px',
+    },
+    hover: {},
+    place: {
+      active: '20px',
+      base: '8px',
     },
   },
 };
