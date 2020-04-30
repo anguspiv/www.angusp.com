9b6cdeb6120a2a0629ed752b6075f059e081b066
Angus Perkerson <angusp@angusp.com>
2020-04-29 23:08:46 -0700

    feat(pages): adds ghost pages
    Adds ghost pages to the build process
    
    closes #61

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -1,58 +1,53 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark;
+function Page({ data }) {
+  const { title, html, meta_title, meta_description, og_image } = data.ghostPage || {};
   return (
     <Layout>
-      <SEO title={frontmatter.title} description={frontmatter.description} />
-      <div
-        className="blog-post-content"
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
+      <SEO title={meta_title} description={meta_description} image={og_image} />
+      <article>
+        <h1>{title}</h1>
+        <section
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </article>
     </Layout>
   );
 }
 
-Template.propTypes = {
+Page.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.shape({
-        title: PropTypes.string,
-        description: PropTypes.string,
-        date: PropTypes.string,
-      }),
+    ghostPage: PropTypes.shape({
+      title: PropTypes.string,
+      feature_image: PropTypes.string,
       html: PropTypes.string,
+      meta_title: PropTypes.string,
+      meta_description: PropTypes.string,
+      og_image: PropTypes.string,
+      reading_time: PropTypes.number,
     }),
   }),
 };
 
-Template.defaultProps = {
-  data: {
-    markdownRemark: {
-      frontmatter: {},
-      html: '',
-    },
-  },
+Page.defaultProps = {
+  data: {},
 };
 
+export default Page;
+
 export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
+  query($slug: String) {
+    ghostPage(slug: { eq: $slug }) {
+      title
+      slug
+      feature_image
       html
-      frontmatter {
-        path
-        title
-        date
-        description
-      }
     }
   }
 `;
