bb66f06c10aef9253db514613fd5351a86ff23c2
Angus <angusp@angusp.com>
2022-01-31 22:36:31 -0800

    style: adds eslint rule for import order
    Turns on the import/order rule for eslint

--- src/components/molecules/PageHeader/PageHeader.test.jsx
+++ src/components/molecules/PageHeader/PageHeader.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
-import theme from '@styles';
 import { axe } from 'jest-axe';
+import theme from '@styles';
 import PageHeader from './PageHeader';
 
 describe('<PageHeader />', () => {
