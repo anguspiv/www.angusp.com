ca99948994beb29e17823015aa11cc974408cab3
Angus <angusp@angusp.com>
2022-02-02 20:54:18 -0800

    feat(articles): adds the featured articles section
    Adds the most recent featured article section to the
    articles landing page

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import Layout from '../../components/templates/Layout';
-import SEO from '../../components/organisms/SEO/SEO';
-import PageHeader from '../../components/molecules/PageHeader';
+import Layout from '@components/templates/Layout';
+import SEO from '@components/organisms/SEO';
+import PageHeader from '@components/molecules/PageHeader';
+import PageSection from '@components/atoms/PageSection';
+import FeaturedArticle from '@components/organisms/FeaturedArticle';
 
 function Articles() {
   return (
     <Layout>
       <SEO title="Articles - Angus Perkerson" />
       <PageHeader title="Articles" />
+      <PageSection>
+        <FeaturedArticle />
+      </PageSection>
     </Layout>
   );
 }
