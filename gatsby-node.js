e1ef76ffb29325228c9ebc3ee52c5f7bf48de0db
Angus <angusp@angusp.com>
2022-02-04 22:25:24 -0800

    feat(articles): adds the article list
    Adds the List of Articles for the articles naviation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,55 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const get = require('lodash/get');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { paginate } = require('gatsby-awesome-pagination');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const featured = await graphql(`
+    {
+      allGhostPost(limit: 1, sort: { order: DESC, fields: created_at }) {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  const featuredId = get(featured, 'data.allGhostPost.edges[0].node.id');
+
+  // Fetch your items (blog posts, categories, etc).
+  const { data } = await graphql(
+    `
+      query postsQuery {
+        allGhostPost(
+          sort: { fields: created_at, order: DESC }
+          filter: { id: { ne: "${featuredId}" } }
+        ) {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `,
+  );
+
+  const posts = get(data, 'allGhostPost.edges', []);
+
+  // Create your paginated pages
+  paginate({
+    createPage, // The Gatsby `createPage` function
+    items: posts, // An array of objects
+    itemsPerPage: 10, // How many items you want per page
+    itemsPerFirstPage: 5,
+    pathPrefix: ({ pageNumber }) => (pageNumber === 0 ? '/articles' : '/articles/page'), // Creates pages like `/blog`, `/blog/2`, etc
+    component: path.resolve('./src/templates/Posts.jsx'),
+    context: {
+      featuredId,
+    },
+  });
+};
