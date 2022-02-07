adce800be93012527f031f4a00afd949b39cdf6d
Angus <angusp@angusp.com>
2022-02-06 23:00:35 -0800

    feat: adds tags pages
    Adds the `/tags/` pages to the site
    
    closes #92, #91

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,9 +3,64 @@ const get = require('lodash/get');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { paginate } = require('gatsby-awesome-pagination');
 
-exports.createPages = async ({ actions, graphql }) => {
+const createTagPages = async (graphql, { createPage }) => {
+  const { data } = await graphql(`
+    {
+      allGhostTag(filter: { visibility: { eq: "public" } }, sort: { fields: name, order: ASC }) {
+        edges {
+          node {
+            slug
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  const tags = get(data, 'allGhostTag.edges', []);
+
+  const tagPages = tags.map(({ node }) => {
+    const { slug, id } = node;
+
+    const promise = graphql(`
+      query postsQuery {
+        allGhostPost(
+          sort: { fields: created_at, order: DESC }
+          filter: { tags: { elemMatch: { id: { eq: "${id}" } } } }
+        ) {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `).then((query) => {
+      const posts = get(query, 'data.allGhostPost.edges', []);
+
+      paginate({
+        createPage, // The Gatsby `createPage` function
+        items: posts, // An array of objects
+        itemsPerPage: 10, // How many items you want per page
+        itemsPerFirstPage: 5,
+        pathPrefix: ({ pageNumber }) => (pageNumber === 0 ? `/tags/${slug}` : `/tags/${slug}/page`), // Creates pages like `/blog`, `/blog/2`, etc
+        component: path.resolve('./src/templates/TagPosts.jsx'),
+        context: {
+          tagId: id,
+        },
+      });
+    });
+
+    return promise;
+  });
+
+  return Promise.all(tagPages);
+};
+
+const createPostPages = async (graphql, actions) => {
   const { createPage } = actions;
 
+  // Create Article Pagination
   const featured = await graphql(`
     {
       allGhostPost(limit: 1, sort: { order: DESC, fields: created_at }) {
@@ -52,4 +107,9 @@ exports.createPages = async ({ actions, graphql }) => {
       featuredId,
     },
   });
+};
+
+exports.createPages = async ({ actions, graphql }) => {
+  await createPostPages(graphql, actions);
+  await createTagPages(graphql, actions);
 };
---
+++ src/templates/TagPosts.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { get } from 'lodash';
+import Layout from '@components/templates/Layout';
+import SEO from '@components/organisms/SEO';
+import PageHeader from '@components/molecules/PageHeader';
+import PageSection from '@components/atoms/PageSection';
+import ArticleList from '@components/organisms/ArticleList';
+import Pagination from '@components/molecules/Pagination';
+import Divider from '@components/atoms/Divider';
+
+function TagPosts({ data, pageContext }) {
+  const edges = get(data, 'allGhostPost.edges', []);
+  const { name, description } = get(data, 'ghostTag', {});
+
+  const posts = edges.map(({ node }) => node);
+  const { previousPagePath, nextPagePath, humanPageNumber, numberOfPages } = pageContext;
+
+  return (
+    <Layout>
+      <SEO title={`Tags - ${name} - Angus Perkerson`} />
+      <PageHeader title={name} excerpt={description} />
+      <PageSection>
+        {!!posts.length && <ArticleList articles={posts} />}
+        {numberOfPages > 1 && (
+          <>
+            <Divider />
+            <Pagination
+              previousPagePath={previousPagePath}
+              nextPagePath={nextPagePath}
+              humanPageNumber={humanPageNumber}
+            />
+          </>
+        )}
+      </PageSection>
+    </Layout>
+  );
+}
+
+TagPosts.propTypes = {
+  data: PropTypes.shape({
+    allGhostPost: PropTypes.shape({
+      edges: ArticleList.propTypes.articles,
+    }),
+  }),
+  pageContext: PropTypes.shape({
+    previousPagePath: PropTypes.string,
+    nextPagePath: PropTypes.string,
+    numberOfPages: PropTypes.number,
+    humanPageNumber: PropTypes.number,
+    tagId: PropTypes.string,
+  }),
+};
+
+TagPosts.defaultProps = {
+  data: {},
+  pageContext: {},
+};
+
+export default TagPosts;
+
+export const query = graphql`
+  query tagPostsQuery($skip: Int!, $limit: Int!, $tagId: String!) {
+    allGhostPost(
+      limit: $limit
+      skip: $skip
+      filter: { tags: { elemMatch: { id: { eq: $tagId } } } }
+      sort: { fields: created_at, order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          created_at(formatString: "MMMM DD, YYYY")
+          excerpt
+          reading_time
+          slug
+          title
+        }
+      }
+    }
+    ghostTag(id: { eq: $tagId }) {
+      name
+      description
+    }
+  }
+`;
