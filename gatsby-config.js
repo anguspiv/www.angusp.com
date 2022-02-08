3c3d3042471dcc5c175cd50022352059b193d022
Angus <angusp@angusp.com>
2022-02-07 22:43:13 -0800

    feat: adds ga tracking to the site
    closes #71

--- gatsby-config.js
+++ gatsby-config.js
@@ -190,5 +190,34 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-force-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-8WWWZ3XLTZ', // Google Analytics / GA
+          'UA-45031912-1',
+          // 'GA-TRACKING_ID', // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'GTM-KVCS6WB',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
+      },
+    },
   ],
 };
