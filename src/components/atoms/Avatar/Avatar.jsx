9ef1172be2f144c67a4d3cc790ca87f35912c5f3
Angus <angusp@angusp.com>
2022-10-27 13:00:51 -0700

    feat(atoms): adds avatar component
    Adds a simple avatar element for styles
    
    closes #139

---
+++ src/components/atoms/Avatar/Avatar.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+import { frame, image } from './styles';
+
+export function Avatar({ className, width, height, ...props }) {
+  return (
+    <div data-testid="avatar" className={className} css={frame}>
+      <Image {...props} layout="fill" css={image} />
+    </div>
+  );
+}
+
+Avatar.propTypes = {
+  ...Image.propTypes,
+  src: PropTypes.oneOfType([PropTypes.string, PropTypes.shape]).isRequired,
+  className: PropTypes.string,
+};
+
+Avatar.defaultProps = {
+  ...Image.defaultProps,
+  className: '',
+};
+
+export default Avatar;
