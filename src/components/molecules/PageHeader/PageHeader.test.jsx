3dc8013c029a7a4e8da74654f6ea77a6da60eb4e
Angus <angusp@angusp.com>
2022-11-04 18:33:33 -0700

    feat(molecules): adds the page header component
    Adds the page header component
    
    closes #141

---
+++ src/components/molecules/PageHeader/PageHeader.test.jsx
@@ -0,0 +1,77 @@
+import { render, screen } from '@testing-library/react';
+import { PageHeader } from './PageHeader';
+
+describe('<PageHeader />', () => {
+  it('should render the page header', () => {
+    expect.assertions(10);
+
+    const location = {
+      pathname: '/somewhere/else-where',
+    };
+
+    const excerpt = 'This is the excerpt';
+
+    render(
+      <PageHeader
+        title="Test Title"
+        location={location}
+        publishDate="Dec. 2, 2010"
+        readingTime={10}
+        tags={[
+          {
+            label: 'Test Tag',
+            slug: 'test-tag',
+            color: '#ff0000',
+          },
+        ]}
+        excerpt={excerpt}
+        featuredImage="https://via.placeholder.com/300"
+      />,
+    );
+
+    expect(screen.getByRole('heading', { name: 'Test Title' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Somewhere' })).toHaveAttribute('href', '/somewhere');
+    expect(screen.getByText('Else Where')).toBeInTheDocument();
+    expect(screen.getByText('Dec. 2, 2010')).toBeInTheDocument();
+    expect(screen.getByText('Reading Time: 10 min.')).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Test Tag' })).toHaveAttribute(
+      'href',
+      '/tags/test-tag',
+    );
+    expect(screen.getByText(excerpt)).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('src');
+    expect(screen.getByRole('img')).toHaveAttribute('alt', '');
+    expect(screen.getByTestId('featured-image')).toHaveStyle({ order: 2 });
+  });
+
+  it('should render the featured image on top', () => {
+    expect.assertions(1);
+
+    const location = {
+      pathname: '/somewhere/else-where',
+    };
+
+    const excerpt = 'This is the excerpt';
+
+    render(
+      <PageHeader
+        title="Test Title"
+        location={location}
+        publishDate="Dec. 2, 2010"
+        readingTime={10}
+        tags={[
+          {
+            label: 'Test Tag',
+            slug: 'test-tag',
+            color: '#ff0000',
+          },
+        ]}
+        excerpt={excerpt}
+        featuredImage="https://via.placeholder.com/300"
+        variant="imageTop"
+      />,
+    );
+
+    expect(screen.getByTestId('featured-image')).toHaveStyle({ order: 1, marginBottom: 0 });
+  });
+});
---
+++ src/components/molecules/PageHeader/index.js
@@ -0,0 +1,5 @@
+import { PageHeader } from './PageHeader';
+
+export * from './PageHeader';
+
+export default PageHeader;
