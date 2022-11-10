783598609b08799c1e88dce6c80fceaafd666c91
Angus <angusp@angusp.com>
2022-11-09 20:44:05 -0800

    feat: adds the app layout to app
    Adds the app layout to the app
    
    closes #148

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?&family=Fira+Code:wght@300..700&family=Open+Sans:wght@300..700&display=optional"
           rel="stylesheet"
