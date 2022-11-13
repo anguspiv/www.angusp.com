366eef3782e7ac267c92d731daab94c48595ede5
Angus <angusp@angusp.com>
2022-11-12 18:02:12 -0800

    feat(posts): adds death to legacy code post
    Adds the Death to Legacy Code Post
    
    closes #180

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import mdx from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')({
+const withMDX = mdx({
   extension: /\.mdx?$/,
   options: {
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
     remarkPlugins: [],
-    rehypePlugins: [],
+    rehypePlugins: [rehypeAutolinkHeadings],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
@@ -24,4 +27,4 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
 
-module.exports = withMDX(nextConfig);
+export default withMDX(nextConfig);
