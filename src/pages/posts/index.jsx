8cdd670fc7d8cd6b4634389c0dc90259d196f3d8
Angus <angusp@angusp.com>
2022-11-13 18:37:31 -0800

    feat: adds the posts index page
    closes #196

---
+++ src/pages/posts/index.jsx
@@ -0,0 +1,60 @@
+import { PropTypes } from 'prop-types';
+import { useRouter } from 'next/router';
+import { css } from '@emotion/react';
+import { spacing } from '@styles/utils';
+import { getAllPosts } from '@lib/api';
+import { Container } from '@components/atoms/Container';
+import { PageHeader } from '@components/molecules/PageHeader';
+import { BreadCrumbs } from '@components/molecules/BreadCrumbs';
+import { FeaturedArticle } from '@components/organisms/FeaturedArticle';
+import { ArticleList } from '@components/organisms/ArticleList';
+
+function Posts({ posts }) {
+  const featuredPost = posts[0];
+  const morePosts = posts.slice(1);
+
+  const location = useRouter();
+
+  return (
+    <>
+      <PageHeader title="Posts" />
+      <Container
+        css={css`
+          margin-bottom: ${spacing(2)};
+        `}
+      >
+        <BreadCrumbs location={location} />
+      </Container>
+      <FeaturedArticle article={featuredPost} />
+      {morePosts.length > 0 && <ArticleList articles={morePosts} title="Recent Posts" />}
+    </>
+  );
+}
+
+Posts.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string,
+      excerpt: PropTypes.string,
+      featured: PropTypes.bool,
+      readingTime: PropTypes.number,
+      slug: PropTypes.string,
+      title: PropTypes.string,
+    }),
+  ),
+};
+
+Posts.defaultProps = {
+  posts: [],
+};
+
+export const getStaticProps = async () => {
+  const posts = await getAllPosts(['title', 'date', 'excerpt', 'tags', 'slug']);
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default Posts;
