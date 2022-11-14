be8163f49630207002cd6061867fb76da15c7324
Angus <angusp@angusp.com>
2022-11-13 20:07:47 -0800

    feat: adds google analytics tag
    closes #190

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -9,6 +10,25 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?&family=Fira+Code:wght@300..700&family=Open+Sans:wght@300..700&display=optional"
           rel="stylesheet"
+        />{' '}
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=302746805"
+        />
+        <Script
+          strategy="afterInteractive"
+          id="gaconfig"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-302746805', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
         />
       </Head>
       <body>
