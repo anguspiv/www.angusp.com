6014fb1a4dc208d5dd1a7853903d42d4cfb1d3df
Angus <angusp@angusp.com>
2022-11-15 19:50:19 -0800

    test: adds cypress testing for about page
    Adds Cypress testing, and the about page
    
    closes #212, #213

---
+++ cypress/e2e/about.cy.js
@@ -0,0 +1,14 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.visit('/about');
+    cy.injectAxe();
+  });
+
+  it('has no a11y violations', () => {
+    cy.checkA11y();
+  });
+
+  it('has a title', () => {
+    cy.findByRole('heading', { name: 'Angus Perkerson' }).should('exist');
+  });
+});
