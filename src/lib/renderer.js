6b17b4223b8a553128a2cc9d73c15558de64b148
Angus <angusp@angusp.com>
2022-11-22 21:50:40 -0800

    fix(styles): add critical css to page renderer
    Adds emotion critical css extration to page generation
    
    closes #226

---
+++ src/lib/renderer.js
@@ -0,0 +1,16 @@
+import createEmotionServer from '@emotion/server/create-instance';
+// eslint-disable-next-line @emotion/no-vanilla
+import { cache } from '@emotion/css';
+
+export const renderStatic = async (html) => {
+  if (html === undefined) {
+    throw new Error('did you forget to return html from renderToString?');
+  }
+
+  const { extractCritical } = createEmotionServer(cache);
+  const { ids, css } = extractCritical(html);
+
+  return { html, ids, css };
+};
+
+export default renderStatic;
