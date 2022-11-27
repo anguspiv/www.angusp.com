23ff58548249b961d318afd3698aecb01d8b08f2
Angus <angusp@angusp.com>
2022-11-26 22:25:16 -0800

    fix: fixes fouc from emotion cache
    Removes the emotion CacheProvider from the client to fix the flash of
    unstyled content
    
    closes #226

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
-import createCache from '@emotion/cache';
-import { CacheProvider, Global, ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import { global } from '@styles/global';
 import { theme } from '@styles/theme';
 import { AppLayout } from '@components/templates/AppLayout';
@@ -8,23 +7,19 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { SEO } from '@components/organisms/SEO';
 import { CMS_HOST } from '@constants/hosts';
 
-const cache = createCache({ key: 'css', prepend: true });
-
 function MyApp({ Component, pageProps }) {
   return (
-    <CacheProvider value={cache}>
-      <ThemeProvider theme={theme}>
-        <Global styles={global} />
-        <AppLayout>
-          <SEO
-            title="Angus Perkerson - Software Engineer and Manager"
-            descriptio="Angus Perkerson is a Software Engineer and Manger who specializes in web application development and team development."
-            image={`${CMS_HOST}/images/angus-perkerson.jpg`}
-          />
-          <Component {...pageProps} />
-        </AppLayout>
-      </ThemeProvider>
-    </CacheProvider>
+    <ThemeProvider theme={theme}>
+      <Global styles={global} />
+      <AppLayout>
+        <SEO
+          title="Angus Perkerson - Software Engineer and Manager"
+          descriptio="Angus Perkerson is a Software Engineer and Manger who specializes in web application development and team development."
+          image={`${CMS_HOST}/images/angus-perkerson.jpg`}
+        />
+        <Component {...pageProps} />
+      </AppLayout>
+    </ThemeProvider>
   );
 }
 
